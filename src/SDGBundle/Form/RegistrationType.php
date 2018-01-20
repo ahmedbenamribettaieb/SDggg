4744a48ed83c866ff269d42c4865d968f18a2190
Ahmed <ahmed.benamribettaieb@esprit.tn>
2018-01-20 15:40:22 +0100

    v1.0

--- src/SDGBundle/Form/RegistrationType.php
+++ src/SDGBundle/Form/RegistrationType.php
@@ -14,21 +14,18 @@ class RegistrationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
 
     {
-
         $builder
             ->add('roles', CollectionType::class, array(
-                'entry_type' => ChoiceType::class,
-                'entry_options' => array(
-                    'choices' => array(
-                        'vous etes un medecin ' => 'ROLE_MEDECIN',
-                        'vous etes une association' => 'ROLE_ASSOCIATION'
-
+                    'entry_type' => ChoiceType::class,
+                    'entry_options' => array(
+                        'choices' => array(
+                            'ROLE_MEDECIN' => 'ROLE_MEDECIN',
+                            'ROLE_ASSOCIATION' => 'ROLE_ASSOCIATION'
                         )
                     )
                 )
             )
         ;
-
 
     }
 
