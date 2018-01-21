da2a9fe397c1bb5fbc7944eee29e2565025547e9
Ahmed <ahmed.benamribettaieb@esprit.tn>
2018-01-21 03:59:44 +0100

    Merge branch 'master' of https://github.com/ahmedbenamribettaieb/SDG
    # Conflicts:
    #	src/SDGBundle/Resources/views/Default/index.html.twig

--- src/SDGBundle/Entity/Sos.php
+++ src/SDGBundle/Entity/Sos.php
@@ -8,10 +8,12 @@
 
 namespace SDGBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
-
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 /**
  * @ORM\Entity
  * @ORM\Table(name="sos")
+ * @Vich\Uploadable
  */
 
 class Sos
@@ -111,7 +113,53 @@ class Sos
     }
 
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    private $image;
 
+    /**
+     * @Vich\UploadableField(mapping="product_images", fileNameProperty="image")
+     * @var File
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @var \DateTime
+     */
+    private $updatedAt;
+
+    // ...
+
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($image) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
 
 
 }
